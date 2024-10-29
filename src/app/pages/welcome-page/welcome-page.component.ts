8482b021c8f0727496b07d458dca905d92484482
IvanVidelaR <videlaivan10@gmail.com>
2024-10-29 20:25:02 -0300

    💄 Mejoro el diseño de la pagina de bienvenida
    Cambio header, footer, y el main content para hacerlo más lindo

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
