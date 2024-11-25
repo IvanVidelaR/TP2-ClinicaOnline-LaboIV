a144481641af110bfa0548da5ab69193de7778e0
IvanVidelaR <videlaivan10@gmail.com>
2024-11-25 18:21:48 -0300

    ✨ Excel y ordenamiento a logs ingreso al sistema
    Excel y ordenamiento a logs ingreso al sistema

--- src/app/pages/estadisticas/log-usuarios/log-usuarios.component.ts
+++ src/app/pages/estadisticas/log-usuarios/log-usuarios.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { LogUsuario } from '../../../models/logUsuario.model';
 import { DatabaseService } from '../../../services/database.service';
 import { DatePipe } from '@angular/common';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-log-usuarios',
@@ -21,12 +22,39 @@ export class LogUsuariosComponent implements OnInit{
   ngOnInit(): void {
     this.loading = true;
     const logsUsuariosSub = this.databaseService.getDocument('logs_usuarios').subscribe((logs) => {
-      logs.forEach((log: any) => {
+      this.logsUsuarios = logs.map((log: any) => {
         log.fecha = this.databaseService.convertTimestampToDate(log.fecha);
-        this.logsUsuarios.push(log);
+        return log;
+      }).sort((a: LogUsuario, b: LogUsuario) => {
+        return new Date(b.fecha).getTime() - new Date(a.fecha).getTime()
       });
+
       this.loading = false;
     });
     this.subscriptions.add(logsUsuariosSub);
+  }
+
+  descargarExcelDatosLogs() {
+    const datos = this.logsUsuarios.map((log: LogUsuario) => ({
+      FechaDeIngreso: log.fecha.toLocaleString(),
+      Perfil: log.usuario.perfil,
+      Nombre: log.usuario.nombre,
+      Apellido: log.usuario.apellido,
+      Edad: log.usuario.edad,
+      DNI: log.usuario.dni,
+      Mail: log.usuario.email,
+      'Imagen de Perfil': log.usuario.imagenDePerfil,
+      'Obra Social': log.usuario.obraSocial || 'NO TIENE',
+      'Imagen de Portada': log.usuario.segundaImagenDePerfil || 'NO TIENE',
+      Especialidades: log.usuario.especialidad ? log.usuario.especialidad.join(', ') : 'TIENE',
+      Habilitado: log.usuario.habilitado !== undefined ? (log.usuario.habilitado ? 'Sí' : 'No') : 'NO TIENE',
+    }));
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(datos);
+
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Logs ingresos al sistema');
+
+    XLSX.writeFile(workbook, 'logsSistema.xlsx');
   }
 }
